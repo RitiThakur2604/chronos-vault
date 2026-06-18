b46f82ddde64be236b63c74d5b94e569e3789c6f
RitiThakur2604 <riti.thakur.min24@itbhu.ac.in>
2026-06-18 10:31:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -109,7 +109,8 @@ app.post('/unlock/:token', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
